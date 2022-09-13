5681a40c96c82cee4f5fd9dd58f0c3ad3cc3a314
neoconstel <neoconstel@gmail.com>
2022-09-13 12:41:53 +0100

    kill all child processes whose assigned processes are dead

--- cpu.py
+++ cpu.py
@@ -20,7 +20,8 @@ while(loop):
 
 	with open(prog_log) as file:
 		lines = file.readlines()[1:]
-		
+	
+	current_pids = []
 	for process_info in lines:
 		process_info = re.sub(re.compile("\s+"), " ", process_info)
 		process_info = process_info.split(" ")
@@ -29,6 +30,8 @@ while(loop):
 		process_cpu = float(process_info[2])
 		process_command = process_info[10]
 		
+		current_pids.append(process_pid)
+
 		if process_cpu > limit:
 			if not process_pid in excluded_pids:
 				# if pid has not been marked as 'limited' or it has been
@@ -46,7 +49,7 @@ while(loop):
 					# execute if parent
 					if child_pid > 0:
 						# mark child process for exclusion to avoid a chain of infinite limits
-						excluded_pids[child_pid] = True
+						excluded_pids[child_pid] = process_pid
 						child_processes += 1
 						print(f"\nChild processes: {child_processes}\n\n")
 					
@@ -54,6 +57,25 @@ while(loop):
 					if child_pid == 0:
 						loop = False
 						break
+	
+	# kill child processes assigned to PIDs no longer active
+
+	# make copy so we can make changes as we iterate through it -- improve this later
+	excluded_pids_ = excluded_pids.copy()
+	for pid, assigned_pid in excluded_pids_.items():
+		if type(assigned_pid) == int and not assigned_pid in current_pids:
+			# kill the child process via pid			
+			os.system(f"kill -9 {pid}")
+			print(f"Killed child process (pid={pid}) tasked with limiting dead process of PID: {assigned_pid}")
+			child_processes -= 1
+			print(f"\nChild processes: {child_processes}\n\n")
+
+			# remove the child pid from excluded pids
+			excluded_pids.pop(pid)
+			
+			# remove the assigned pid from limited pids
+			limited_pids.pop(assigned_pid)
+
 				
 	# print("\n\n")
 		
